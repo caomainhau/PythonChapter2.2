7a35e6f7a398ee87cd7b989e7c7bbb47bab3790c
caomainhau <manhhaucao@gmail.com>
2025-09-23 15:11:44 +0700

    OK

---
+++ Bai1.py
@@ -0,0 +1,5 @@
+num = int(input("Nhập số: "))
+if num % 2 == 0:
+    print("Đây là số chẵn")
+else:
+    print("Đầy là số lẻ")
---
+++ Bai2.py
@@ -0,0 +1,7 @@
+chuoi = input("Nhập chuỗi: ")
+if chuoi.isupper():
+    print("Chuoi la chu hoa")
+elif chuoi.islower():
+    print("Chuoi la chu thuong")
+else:
+    print("Chuoi chua chu thuong va chu hoa")

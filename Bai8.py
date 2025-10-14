cb090afbfe31d2206ceb62ae3552316a646afe80
caomainhau <manhhaucao@gmail.com>
2025-10-14 12:21:08 +0700

    OK

--- Bai8.py
+++ Bai8.py
@@ -1,7 +1,5 @@
-# Định nghĩa chuỗi các ký tự cần loại bỏ
 st1 = "!()-[]{};:'\",<>./?@#$%^&*_~"
 my_str = input("Nhập chuỗi: ")
-# Loại bỏ các ký tự đặc biệt
 st2 = ""
 for char in my_str:
     if char not in st1:

e50e031fa2ab7492e5cd1cf47da050c50d40b038
caomainhau <manhhaucao@gmail.com>
2025-10-20 20:45:21 +0700

    OK

---
+++ Bai21.py
@@ -0,0 +1,6 @@
+def Tao_In_DS():
+    ds=list()
+    for i in range(1,21):
+        ds.append(i**2)
+    print(ds)
+Tao_In_DS()
---
+++ Bai23_Test.py
@@ -0,0 +1,3 @@
+from Bai23_Kiem_tra import Check_chan
+n = int(input("Nhập vào một số để kiểm tra: "))
+Check_chan(n)

a174bdec33d0accb991a6e9995b25d423ea76098
caomainhau <manhhaucao@gmail.com>
2025-10-20 20:37:20 +0700

    OK

---
+++ Bai17.py
@@ -0,0 +1,13 @@
+chuoi = input("Nhập vào một chuỗi: ")
+d = {"chu_cai": 0, "chu_so": 0}
+
+for ch in chuoi:
+    if ch.isalpha():
+        d["chu_cai"] += 1
+    elif ch.isdigit():
+        d["chu_so"] += 1
+    else:
+        pass
+
+print("Số chữ cái là:", d["chu_cai"])
+print("Số chữ số là:", d["chu_so"])
